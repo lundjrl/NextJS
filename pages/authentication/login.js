5ae679025d62e1d90f218a6736d840ff409d5dc0
lundjrl <jrlbidamin@gmail.com>
2021-08-14 14:14:47 -0400

    Init

---
+++ pages/authentication/login.js
@@ -0,0 +1,10 @@
+
+
+const Login = () => (
+    <div>
+       <strong>This is a login screen.</strong> 
+        <p>Your current route is <code>/authentication/login</code></p>
+    </div>
+)
+
+export default Login
